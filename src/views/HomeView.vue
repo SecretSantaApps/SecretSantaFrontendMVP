f50882b9d0875ced727653e31a1373e6a16fce68
Vladislav Kheynov <lcastory86@gmail.com>
2023-02-27 16:11:56 +0700

    chore: change home screen design

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -12,6 +12,6 @@
 <template>
   <main class="w-full h-full flex flex-col justify-center items-center space-y-10">
     <h1 class="text-2xl">Hello!</h1>
-    <button @click="router.push('/login')">Login</button>
+    <button @click="router.push('/login')" class="py-4 px-2 rounded-full bg-amber-50 text-xl">Login</button>
   </main>
 </template>
